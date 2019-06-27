10e56837f06d3862aacff72fa13cf5734267f642
Albert-Ubun <dorcealbertmary@gmail.com>
2019-06-27 10:07:01 -0400

    Initial commit

---
+++ app/src/main/java/com/Albert/topquiz/controller/GameActivity.java
@@ -0,0 +1,29 @@
+package com.Albert.topquiz.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.Albert.topquiz.R;
+
+public class GameActivity extends AppCompatActivity {
+    private TextView questionText;
+    private Button answer1;
+    private Button answer2;
+    private Button answer3;
+    private Button answer4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+        questionText = (TextView)findViewById(R.id.question);
+        answer1 = (Button)findViewById(R.id.answer1);
+        answer2 = (Button)findViewById(R.id.answer2);
+        answer3 = (Button)findViewById(R.id.answer3);
+        answer4 = (Button)findViewById(R.id.answer4);
+
+    }
+}
